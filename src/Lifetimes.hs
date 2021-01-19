041034cb693636609fbff1075525c18e8f8b66ae
Ian Denhardt <ian@zenhack.net>
2021-01-19 14:08:49 -0500

    Initial commit.
    Last test doesn't build; need to define Functor/Applicative/Monad
    instances for Acquire.

---
+++ src/Lifetimes.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE DeriveFunctor  #-}
+{-# LANGUAGE NamedFieldPuns #-}
+module Lifetimes
+    ( Acquire
+    , Resource
+    , makeAcquire
+    , newLifetime
+    , withAcquire
+    , withLifetime
+    , acquire
+    , acquireValue
+    , moveTo
+    , moveToSTM
+    , getResource
+    ) where
+
+import           Control.Concurrent.STM
+import           Control.Exception          (bracket, finally)
+import           Control.Monad.Trans.Reader (ReaderT, ask, runReaderT)
+import           Data.Foldable              (fold)
+import qualified Data.Map.Strict            as M
+import           Data.Maybe                 (fromJust)
+import           Zhp
+
+newtype ReleaseKey = ReleaseKey Word64
+    deriving(Show, Read, Ord, Eq, Bounded)
+
+instance Enum ReleaseKey where
+    toEnum n = ReleaseKey (toEnum n)
+    fromEnum (ReleaseKey n) = fromEnum n
+
+newtype Cleanup = Cleanup { runCleanup :: IO () }
+
+instance Semigroup Cleanup where
+    -- We want resources to be released in the opposite order from their
+    -- acquisition, so x <> y releases y and then x.
+    Cleanup x <> Cleanup y = Cleanup $ y `finally` x
+
+instance Monoid Cleanup where
+    mempty = Cleanup $ pure ()
+
+data Lifetime = Lifetime
+    { resources      :: TVar (M.Map ReleaseKey Cleanup)
+    , nextReleaseKey :: TVar ReleaseKey
+    }
+
+data Resource a = Resource
+    { releaseKey :: TVar ReleaseKey
+    , lifetime   :: TVar Lifetime
+    , value      :: a
+    }
+    deriving(Functor)
+
+newtype Acquire a = Acquire (ReaderT Lifetime IO (Resource a))
+
+newReleaseKey :: Lifetime -> STM ReleaseKey
+newReleaseKey Lifetime{nextReleaseKey} = do
+    key <- readTVar nextReleaseKey
+    writeTVar nextReleaseKey $! succ key
+    pure key
+
+acquire1 :: Lifetime -> IO a -> (a -> IO ()) -> IO (Resource a)
+acquire1 lt@Lifetime{resources} get clean = do
+    bracket
+        (get >>= newTVarIO . Just)
+        (\var -> atomically (readTVar var) >>= traverse_ clean)
+        (\var -> atomically $ do
+            key <- newReleaseKey lt
+            value <- fromJust <$> readTVar var
+            modifyTVar resources $
+                M.insert key (Cleanup (clean value))
+            writeTVar var Nothing
+            lifetime <- newTVar lt
+            releaseKey <- newTVar key
+            pure Resource
+                { releaseKey
+                , lifetime
+                , value
+                }
+        )
+
+makeAcquire :: IO a -> (a -> IO ()) -> Acquire a
+makeAcquire get clean = Acquire $ do
+    lt <- ask
+    liftIO $ acquire1 lt get clean
+
+newLifetime :: Acquire Lifetime
+newLifetime = makeAcquire createLifetime destroyLifetime
+
+createLifetime :: IO Lifetime
+createLifetime = Lifetime
+    <$> newTVarIO M.empty
+    <*> newTVarIO minBound
+
+destroyLifetime :: Lifetime -> IO ()
+destroyLifetime Lifetime{resources} =
+    join $ atomically $ do
+        clean <- fold <$> readTVar resources
+        writeTVar resources M.empty
+        pure $ runCleanup clean
+
+withAcquire :: Acquire a -> (a -> IO b) -> IO b
+withAcquire acq use = withLifetime $ \lt -> do
+    res <- acquire lt acq
+    use (getResource res)
+
+withLifetime :: (Lifetime -> IO a) -> IO a
+withLifetime = bracket createLifetime destroyLifetime
+
+acquire :: Lifetime -> Acquire a -> IO (Resource a)
+acquire lt (Acquire acq) = do
+    lt' <- acquire1 lt createLifetime destroyLifetime
+    value' <- getResource <$> runReaderT acq (value lt')
+    pure lt' { value = value' }
+
+acquireValue :: Lifetime -> Acquire a -> IO a
+acquireValue lt acq = getResource <$> acquire lt acq
+
+moveTo :: Resource a -> Lifetime -> IO ()
+moveTo r l = atomically $ moveToSTM r l
+
+moveToSTM :: Resource a -> Lifetime -> STM ()
+moveToSTM r newLifetime = do
+    oldKey <- readTVar $ releaseKey r
+    oldLifetime <- readTVar $ lifetime r
+    oldMap <- readTVar $ resources oldLifetime
+    case M.lookup oldKey oldMap of
+        Nothing -> pure () -- already freed.
+        Just clean -> do
+            modifyTVar (resources oldLifetime) $ M.delete oldKey
+            newKey <- newReleaseKey newLifetime
+            writeTVar (releaseKey r) $! newKey
+            modifyTVar (resources newLifetime) $ M.insert newKey clean
+
+getResource :: Resource a -> a
+getResource = value
---
+++ tests/Main.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main (main) where
+
+import Control.Exception.Safe (SomeException, throwString, try)
+import Data.IORef
+import Lifetimes
+import Test.Hspec
+import Zhp
+
+main :: IO ()
+main = hspec $ do
+    describe "withAcquire" $ do
+        it "Should run the handler on success" $ do
+            ref <- newIORef 0
+            withAcquire (makeAcquire (pure ()) (\() -> writeIORef ref 1)) $ \_ -> pure ()
+            value <- readIORef ref
+            value `shouldBe` 1
+        it "Should run the handler on exceptions" $ do
+            ref <- newIORef 0
+            result <- try $ withAcquire (makeAcquire (pure ()) (\() -> writeIORef ref 1)) $ \() ->
+                throwString "Error"
+            case result of
+                Right () -> error "Should have thrown an exception"
+                Left (_ :: SomeException) -> do
+                    value <- readIORef ref
+                    value `shouldBe` 1
+        it "Should run cleanup handlers in reverse order" $ do
+            ref <- newIORef []
+            withAcquire
+                (do
+                    makeAcquire
+                        (pure ())
+                        (\() -> modifyIORef ref (<>[1]))
+                    makeAcquire
+                        (pure ())
+                        (\() -> modifyIORef ref (<>[2]))
+                    makeAcquire
+                        (pure ())
+                        (\() -> modifyIORef ref (<>[3]))
+                    pure ()
+                )
+                $ \() -> pure ()
+            value <- readIORef ref
+            value `shouldBe` [3,12]
