5529dbfb20b3a8516487f3723f085c64e5a69c86
Ian Denhardt <ian@zenhack.net>
2023-02-04 22:47:05 -0500

    Wait on tasks after cancelling them.

--- src/Lifetimes/Async.hs
+++ src/Lifetimes/Async.hs
@@ -2,11 +2,11 @@
 -- Description: Lifteimes integration for the async package.
 module Lifetimes.Async (acquireAsync) where
 
-import Control.Concurrent.Async (Async, async, cancel, wait)
+import Control.Concurrent.Async (Async, async, cancel, waitCatch)
 import Lifetimes
 import Zhp
 
 -- | Spawn an async task. When it is time to reclaim the resource, 'cancel'
--- will be called.
+-- will be called, and the task will be waited on.
 acquireAsync :: IO a -> Acquire (Async a)
-acquireAsync io = mkAcquire (async io) cancel
+acquireAsync io = mkAcquire (async io) (\a -> cancel a <* waitCatch a)
